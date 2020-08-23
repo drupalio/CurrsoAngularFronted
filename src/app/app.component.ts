135bc92bccb2e6a9b94c8ff265944dc1faebfa6e
Ricardo Morales Paredes <ricardo.morales@globant.com>
2020-08-23 14:40:20 -0500

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'clientes-app';
+  title:string = 'Bienvenido Angular';
+  curso:string = 'Curso Spring 5 con Angular';
+  profesor:string = 'Andres Guzman';
 }
